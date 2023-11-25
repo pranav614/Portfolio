51814c94bd9bab077592f104d67d11648d5f7f50
pranav614 <pranavreddy614@gmail.com>
2023-11-25 11:57:56 +0530

    click

---
+++ index.js
@@ -0,0 +1,15 @@
+let mobileNav=document.querySelector(".navSubMobile");
+let closeDiv=document.querySelector(".close");
+let menu=document.querySelector(".menu");
+const openBar=()=>{
+mobileNav.style.display="flex";
+menu.style.display="none";
+
+}
+const closeFunc=()=>{
+    mobileNav.style.display="none";
+    menu.style.display="block";
+};
+
+console.log("hello");
+
