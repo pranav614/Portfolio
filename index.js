929f4a332c5af0fb5730eda96440cc43f3b822f1
pranav614 <pranavreddy614@gmail.com>
2023-11-29 16:47:01 +0530

    click

--- index.js
+++ index.js
@@ -13,3 +13,21 @@ const closeFunc=()=>{
 
 console.log("hello");
 
+     document.addEventListener("DOMContentLoaded", function () {
+    var contentHeight = document.getElementById("content").offsetHeight;
+    document.getElementById("particles-js").style.height = contentHeight + "px";
+  });
+
+  particlesJS('particles-js', {
+    particles: {
+        number: { value: 80, density: { enable: true, value_area: 800 } },
+        color: { value: '#B20F3F' },  // Adjusted particle color
+        shape: { type: 'circle', stroke: { width: 0, color: '#000000' }, polygon: { nb_sides: 5 } },
+        opacity: { value: 0.6, random: false, anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false } },
+        size: { value: 3, random: true, anim: { enable: false, speed: 40, size_min: 0.1, sync: false } },
+        line_linked: { enable: true, distance: 150, color: '#B20F3F', opacity: 0.6, width: 1 },  // Adjusted line color and opacity
+        move: { enable: true, speed: 6, direction: 'none', random: false, straight: false, out_mode: 'out', bounce: false, attract: { enable: false, rotateX: 600, rotateY: 1200 } }
+    },
+    interactivity: { detect_on: 'canvas', events: { onhover: { enable: true, mode: 'repulse' }, onclick: { enable: true, mode: 'push' }, resize: true }, modes: { grab: { distance: 400, line_linked: { opacity: 1 } }, bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 }, repulse: { distance: 200, duration: 0.4 }, push: { particles_nb: 4 }, remove: { particles_nb: 2 } } },
+    retina_detect: true
+});
