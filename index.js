8af201f1b3510e9d453f32c188060a745033b0a6
pranav614 <pranavreddy614@gmail.com>
2023-12-07 01:41:24 +0530

    click

--- index.js
+++ index.js
@@ -1,96 +1,17 @@
 
-
-
-particlesJS('particles-js', {
-    particles: {
-        number: { value: window.innerWidth < 768 ? 50 : 80, density: { enable: true, value_area: 800 } },
-        color: { value: '#ff0000' },
-        shape: { type: 'circle', stroke: { width: 0, color: '#ff0000' }, polygon: { nb_sides: 5 } },
-        opacity: { value: 0.5, random: false, anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false } },
-        size: { value: 2, random: true, anim: { enable: false, speed: 40, size_min: 0.1, sync: false } },
-        line_linked: { enable: true, distance: 200, color: '#ff0000', opacity: 0.4, width: 2 },
-        move: {
-            enable: true,
-            speed: 5,
-            direction: 'none',
-            random: false,
-            straight: false,
-            out_mode: 'out',
-            bounce: false,
-            attract: { enable: false, rotateX: 600, rotateY: 1200 }
-        }
-    },
-    interactivity: {
-        detect_on: 'canvas',
-        events: {
-            onhover: { enable: true, mode: 'repulse' },
-            onclick: { enable: true, mode: 'push' },
-            resize: true
-        },
-        modes: {
-            grab: { distance: 400, line_linked: { opacity: 1 } },
-            bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-            repulse: { distance: 100, duration: 0.4 },
-            push: { particles_nb: 4 },
-            remove: { particles_nb: 2 }
-        }
-    },
-    retina_detect: true
-});
-
-window.addEventListener('resize', function () {
-    particlesJS('particles-js', {
-        particles: {
-            number: { value: window.innerWidth < 768 ? 50 : 80, density: { enable: true, value_area: 800 } },
-            color: { value: '#ff0000' },
-            shape: { type: 'circle', stroke: { width: 0, color: '#ff0000' }, polygon: { nb_sides: 5 } },
-            opacity: { value: 0.5, random: false, anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false } },
-            size: { value: 2, random: true, anim: { enable: false, speed: 40, size_min: 0.1, sync: false } },
-            line_linked: { enable: true, distance: 200, color: '#ff0000', opacity: 0.4, width: 2 },
-            move: {
-                enable: true,
-                speed: 5,
-                direction: 'none',
-                random: false,
-                straight: false,
-                out_mode: 'out',
-                bounce: false,
-                attract: { enable: false, rotateX: 600, rotateY: 1200 }
-            }
-        },
-        interactivity: {
-            detect_on: 'canvas',
-            events: {
-                onhover: { enable: true, mode: 'repulse' },
-                onclick: { enable: true, mode: 'push' },
-                resize: true
-            },
-            modes: {
-                grab: { distance: 400, line_linked: { opacity: 1 } },
-                bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-                repulse: { distance: 100, duration: 0.4 },
-                push: { particles_nb: 4 },
-                remove: { particles_nb: 2 }
-            }
-        },
-        retina_detect: true
-    });
-});
-
-
-
-let mobileNav = document.querySelector(".navSubMobile");
+let mobileNavbar = document.querySelector(".navSubMobile");
 let closeDiv = document.querySelector(".close");
 let menu = document.querySelector(".menu");
 
+
 const openBar = () => {
-    mobileNav.style.display = "flex";
+    mobileNavbar.style.display = "flex";
     menu.style.display = "none";
 }
 
 const closeFunc = () => {
-    mobileNav.style.display = "none";
+    mobileNavbar.style.display = "none";
     menu.style.display = "block";
 };
 
-console.log("hello");
+
